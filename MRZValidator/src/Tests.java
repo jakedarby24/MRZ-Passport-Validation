b28c16d73fd5e66eb51d65fa95efdd7ae094cf16
Jake Darby <78798337+jakedarby24@users.noreply.github.com>
2022-05-24 16:55:05 +0100

    Tests nearly complete

--- MRZValidator/src/Tests.java
+++ MRZValidator/src/Tests.java
@@ -8,6 +8,7 @@ public class Tests {
     public Args newArgs = new Args();
     public final MRZValidator validateClass = new MRZValidator();
 
+    // Tests whether the correct output is achieved when a mismatching DOB is inputted.
     @Test
     public void testMRZIncorrectDOB() {
         newArgs.dateOfBirth = "270819";
@@ -33,6 +34,13 @@ public class Tests {
     public void testMRZNonNumericDOB() {
         newArgs.dateOfBirth = "A-'s]{";
         newArgs.expiryDate = "160101"; newArgs.nationality = "GBR"; newArgs.passportNumber = "862281640";
+        assertEquals(false, MRZValidator.validatePassport(newArgs, newMRZ));
+    }
+
+    @Test
+    public void testMRZNonNumericExpiry() {
+        newArgs.expiryDate = "__[]=\\aBp";
+        newArgs.dateOfBirth = "910711"; newArgs.nationality = "GBR"; newArgs.passportNumber = "862281640";
         assertEquals(false, MRZValidator.validatePassport(newArgs, newMRZ));
     }
 }
